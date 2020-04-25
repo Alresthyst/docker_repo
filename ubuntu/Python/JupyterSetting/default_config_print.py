1bc3bb304c942fdb20bbdda01137a26ff6b34439
kwk1602 <kwk1602@gmail.com>
2020-04-25 20:34:06 +0900

    Edit jupyter token

--- ubuntu/Python/JupyterSetting/default_config_print.py
+++ ubuntu/Python/JupyterSetting/default_config_print.py
@@ -3,5 +3,5 @@
 
 print("\n" * 3, flush=True)
 print("=" * 50, "\n")
-print("Default jupyter password is 'root'", "\n")
+print("Default jupyter token is 'alresthyst_token'", "\n")
 print("=" * 50, "\n" * 3)
